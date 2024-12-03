aac1696d8b222a0aac3f28d45e0ec8460c64fb80
cagla.boynuegri <cagla.boynuegri@scalefocus.com>
2024-12-03 09:04:34 +0300

    added customer controller

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+@Service
+public class CustomerServiceImpl implements CustomerService{
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder().customerName("cagla").id(UUID.randomUUID()).build();
+    }
+}
